2feafa5f0a5f96cb6cc839d353f23fd67ff7fa1d
Dwight <sajandktm@gmail.com>
2020-06-12 13:35:13 +0545

    ggh

--- test2.py
+++
@@ -1,5 +0,0 @@
-print("HI hi Hi HA HA hA")
-print("SAJjan")
-print("asjkfghasdjkf sjkghfdjsghhfdgkjs j jsffhgkjshfdghdskjd hfdshgjkhfdskj ghjdfkgh kjsh gjksdhfjkghuhgiufhgiuhsdfjkgh kjsgiudhiguhfdkjg jkerrgiusergiuaeigbqerjghuioegkjdfgioerrgbadslkbvmdffo igjrrieogiujedrhg ikuerdgdflh bodf")
-print("Sajjan Boy buddy")
-print("HIHIHIHIHIHIHIHIHIHIHIHIHIHIHI")
